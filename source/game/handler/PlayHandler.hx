f2d9404726bfa61e15abf926fe9a21bf6b6df369
Eric W. Lund <github@egotron.com>
2014-04-26 17:46:24 -0400

    Implemented a piece of Nape.

--- source/game/handler/PlayHandler.hx
+++ source/game/handler/PlayHandler.hx
@@ -8,19 +8,46 @@ import flaxen.service.InputService;
 import flaxen.component.*;
 import flaxen.common.Easing;
 import flaxen.common.LoopType;
+ 
+import nape.geom.Vec2;
+import nape.phys.Body;
+import nape.phys.BodyType;
+import nape.shape.Circle;
+import nape.shape.Polygon;
+import nape.space.Space;
+
+import ash.core.Node;
+
+#if nape
+	#if flash
+		import nape.util.BitmapDebug; 
+	#else
+		import nape.util.ShapeDebug;
+	#end
+	import nape.util.Debug;
+#end
 
 class PlayHandler extends FlaxenHandler
 {
 	public var f:Flaxen;
+  	public var nape:Space;
+  	#if nape public var napeDebug:Debug; #end
+
 
 	public function new(f:Flaxen)
 	{
 		super();
 		this.f = f;
+		var gravity = Vec2.weak(0, 600);
+		nape = new Space(gravity);
 	}
 
 	override public function start(_)
-	{
+	{ 
+		var floor = new Body(BodyType.STATIC);
+		floor.shapes.add(new Polygon(Polygon.rect(0, f.baseHeight - 40, 600, 40)));
+		floor.space = nape;
+
 		f.newSingleton("wall")
 			.add(new Layer(100))
 			.add(Position.zero())
@@ -32,15 +59,43 @@ class PlayHandler extends FlaxenHandler
 			.add(new Image("art/floor.png"));
 
 		f.newComponentSet("climberSet")
-			.add(new Layer(20));
+			.add(new Layer(20))
+			.add(new Rotation(0))
+			.add(Offset.center())
+			.add(Origin.center());
 
-		var bodyPos = new Position(308, 475);
+		// Nape Body
+		var bodyPos = new Position(308, 0);
+		var body = new Body(BodyType.DYNAMIC);
+		body.shapes.add(new Polygon(Polygon.box(63, 90)));
+		body.position.setxy(bodyPos.x, bodyPos.y);
+		body.angularVel = -1;
+		body.space = nape;
 		f.newSetSingleton("climberSet", "body")
 			.add(new Image("art/climber/body.png"))
-			.add(Offset.center())
-			.add(Origin.center())
-			.add(bodyPos);
+			.add(bodyPos)
+			.add(body); 
 
+		// Nape Head
+		// var bodyPos = new Position(308, 475);
+		// var body = new Body(BodyType.DYNAMIC);
+		// body.shapes.add(new Circle(60));
+		// body.position.setxy(bodyPos.x, bodyPos.y);
+		// body.space = nape;
+		// f.newSetSingleton("climberSet", "body")
+		// 	.add(new Image("art/climber/body.png"))
+		// 	.add(bodyPos)
+		// 	.add(body); // Nape entity
+
+
+		#if nape
+        napeDebug = new #if flash BitmapDebug #else ShapeDebug #end (
+        	com.haxepunk.HXP.width, com.haxepunk.HXP.height, com.haxepunk.HXP.stage.color);
+        napeDebug.display.scrollRect = null;
+        com.haxepunk.HXP.stage.addChild(napeDebug.display);
+        #end
+
+/*
 		var rot = new Rotation(-20);
 		var tween = new Tween(rot, { angle:20 }, 0.5, Easing.easeInOutQuad);
 		tween.loop = LoopType.Both;
@@ -90,13 +145,38 @@ class PlayHandler extends FlaxenHandler
 			.add(forearmRot)
 			.add(handPos)
 			.add(new RelativePosition(handPos, forearmPos, new Position(34, 0)));
-
+*/
 	}
 
 	override public function update(_)
 	{
+		nape.step(com.haxepunk.HXP.elapsed);
+
+		#if nape // Nape debug
+			napeDebug.clear();
+			napeDebug.draw(nape);
+			napeDebug.flush();
+		#end
+
+		for(node in f.ash.getNodeList(NapeBodyNode))
+		{
+			node.position.x = node.body.position.x;
+			node.position.y = node.body.position.y;
+			if(node.entity.has(Rotation))
+			{
+				var rot = node.entity.get(Rotation);
+				rot.angle = node.body.rotation * 180 / Math.PI;
+			}
+		}
+
 		var key = InputService.lastKey();
 		//...
 		InputService.clearLastKey();
 	}
+}
+
+class NapeBodyNode extends Node<NapeBodyNode>
+{
+	public var body:Body;
+	public var position:Position;
 }
